3a9678dc3254f8133de2919b2d123d316c3970af
Prasaja <64126967+Prasaja28@users.noreply.github.com>
2021-04-10 20:24:50 +0700

    file ku

---
+++ Main.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg151911513018_minggu6;
+
+import java.io.File;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Prasaja
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        System.out.println("balap 1");
+//        OlahFile a = new OlahFile("docq\\balap 1.txt");
+//        a.prosesUtama();
+        
+        OlahFile b = new OlahFile("docq\\basket.txt");
+        b.prosesUtama();
+        
+//        OlahFile c = new OlahFile("docq\\sepakbola.txt");
+//        c.prosesUtama();
+        
+//        OlahFile d = new OlahFile("docq\\tenis.txt");
+//        d.prosesUtama();
+        
+//        OlahFile b = new OlahFile("docq\\Ekonomi.txt");
+//        b.prosesUtama();
+        
+
+//sebelumnya
+//        System.out.println("Input = ");
+//        OlahFile a = new OlahFile("docq\\balap 1.txt");
+//        a.prosesUtama();
+//        
+//        System.out.println("Output = ");
+//        OlahFile bData=new OlahFile();
+//            OlahFile oFile=new OlahFile();
+//            File file = new File("docq\\balap 1.txt");
+//            String hasil=bData.readDoc(file);
+//            ArrayList hasOlah = oFile.pecahDocJadiKalimat(hasil);
+//            oFile.tampilArrayList(hasOlah);
+        
+//        OlahFile a = new OlahFile("docq\\Ekonomi.txt");
+//        a.prosesUtama();
+//    
+//        OlahFile bData=new OlahFile();
+//            OlahFile oFile=new OlahFile();
+//            File file = new File("docq\\Ekonomi.txt");
+//            String hasil=bData.readDoc(file);
+//            ArrayList hasOlah = oFile.pecahDocJadiKalimat(hasil);
+//            oFile.tampilArrayList(hasOlah);
+        
+
+    
+    }
+    
+}
